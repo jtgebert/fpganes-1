0f1aceaf9ed3381ac5991cebae7374ed87c56220
Jonathan Ebert <jtgebert@gmail.com>
2017-04-04 19:05:36 -0500

    Added updated mif maker and mif files

--- loader2/nes2mif.java
+++ loader2/nes2mif.java
@@ -6,24 +6,27 @@ public class nes2mif {
 
   public static void main(String[] args) {
 
-    if (args.length != 2) {
+    if (args.length != 3) {
       System.err.println("Incorrect arguements\nPlease use: \"java nes2mif"
-                         + " <infile> <outfile>\"");
+                         + " <infile> <outfile1> <outfile2>\"");
       System.exit(-1);
     }
 
-    String inPath, progOutPath;
+    String inPath, progOutPath, charOutPath;
     inPath = args[0];
     progOutPath = args[1];
+    charOutPath = args[2];
 
     try {
       File infile = new File(inPath);
       File progOutfile = new File(progOutPath);
+      File charOutfile = new File(charOutPath);
 
       byte[] bytes = new byte[(int) infile.length()];
       InputStream bytesIn = new BufferedInputStream(new FileInputStream(infile));
 
       PrintWriter progRomPrinter = new PrintWriter(progOutfile);
+      PrintWriter charRomPrinter = new PrintWriter(charOutfile);
 
       try {
         bytesIn.read(bytes, 0, (int) infile.length());
@@ -34,58 +37,41 @@ public class nes2mif {
         System.exit(-1);
       }
 
-      int progRomEnd; // the ending byte of the character rom
+      int progRomLength; // the ending byte of the character rom
       int charRomStart;
       int charRomLength;
 
       // determine prog and char rom size
-      progRomEnd = 32783; // currently only support mmc0
-      charRomStart = 262160;
+      progRomLength = 32768; // currently only support mmc0
       charRomLength = 8192;
 
       // print mif headers to the output files
-      progRomPrinter.println("DEPTH = 266248;\nWIDTH = 16;\nADDRESS_RADIX = HEX;"
+      progRomPrinter.println("DEPTH = 32768;\nWIDTH = 16;\nADDRESS_RADIX = HEX;"
+                              + "\nDATA_RADIX = BIN;\nCONTENT\nBEGIN\n");
+      charRomPrinter.println("DEPTH = 8192;\nWIDTH = 16;\nADDRESS_RADIX = HEX;"
                               + "\nDATA_RADIX = BIN;\nCONTENT\nBEGIN\n");
 
       // loop over all bytes and print them to correct rom file
-      for (int address = 0; address < charRomStart + charRomLength / 2; address++) {
-
-        // use header to determine char and prog rom size
-        if (address <= 15) {
-          // these bytes are for the ines header
-        }
+      for (int address = 0; address < progRomLength / 2; address++) {
         // write prog rom file
-        else if (address <= progRomEnd / 2 + 8) {
-          progRomPrinter.print(String.format("%6X", (address - 16)).replace(" ", "0"));
-          progRomPrinter.print(" : ");
-          progRomPrinter.print(String.format("%8s", Integer.toBinaryString(((int) bytes[address * 2 - 16]) & 0xFF)).replace(' ', '0'));
-          progRomPrinter.println(String.format("%8s;", Integer.toBinaryString(((int) bytes[address * 2 - 15]) & 0xFF)).replace(' ', '0'));
-        }
-        else if (address < charRomStart) {
-          progRomPrinter.print(String.format("%6X", (address - 16)).replace(" ", "0"));
-          progRomPrinter.print(" : ");
-          progRomPrinter.println("0000000000000000;");
-        }
-        // write char rom file
-        else if (address <= charRomStart + charRomLength / 2) {
-          progRomPrinter.print(String.format("%6X", (address - 16)).replace(" ", "0"));
-          progRomPrinter.print(" : ");
-          progRomPrinter.print(String.format("%8s", Integer.toBinaryString(((int) bytes[(address - charRomStart) * 2 + progRomEnd + 1] & 0xFF))).replace(' ', '0'));
-          progRomPrinter.println(String.format("%8s;", Integer.toBinaryString(((int) bytes[(address - charRomStart) * 2 + progRomEnd + 2] & 0xFF))).replace(' ', '0'));
-        }
-
-        // print to console
-        /*
-        System.out.print(String.format("%6X", address).replace(" ", "0"));
-        System.out.print(" : ");
-        System.out.print(String.format("%8s", Integer.toBinaryString(((int) bytes[address]) & 0xFF)).replace(' ', '0'));
-        System.out.println(String.format("%8s;", Integer.toBinaryString(((int) bytes[address + 1]) & 0xFF)).replace(' ', '0'));
-        */
+        progRomPrinter.print(String.format("%6X", (address)).replace(" ", "0"));
+        progRomPrinter.print(" : ");
+        progRomPrinter.print(String.format("%8s", Integer.toBinaryString(((int) bytes[address * 2 + 16]) & 0xFF)).replace(' ', '0'));
+        progRomPrinter.println(String.format("%8s;", Integer.toBinaryString(((int) bytes[address * 2 + 17]) & 0xFF)).replace(' ', '0'));
       }
 
-	  progRomPrinter.print("END");
-	  
+      for (int address = 0; address < charRomLength / 2; address++) {
+        charRomPrinter.print(String.format("%6X", (address)).replace(" ", "0"));
+        charRomPrinter.print(" : ");
+        charRomPrinter.print(String.format("%8s", Integer.toBinaryString(((int) bytes[(address * 2 + progRomLength + 16)] & 0xFF))).replace(' ', '0'));
+        charRomPrinter.println(String.format("%8s;", Integer.toBinaryString(((int) bytes[(address * 2 + progRomLength + 17)] & 0xFF))).replace(' ', '0'));
+      }
+
+	  progRomPrinter.print("END;");
+    charRomPrinter.print("END;");
+
       progRomPrinter.close();
+      charRomPrinter.close();
 
     }
     catch (FileNotFoundException fnfe) {
